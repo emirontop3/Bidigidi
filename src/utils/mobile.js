b101657e910fa197b228fc50866eca6d5f0b98d2
emirontop3 <ozl.kpz@gmail.com>
2026-05-08 20:50:06 +0300

    Create mobile.js

---
+++ src/utils/mobile.js
@@ -0,0 +1,10 @@
+export function setupMobile(controls) {
+
+    controls.enablePan = true
+    controls.enableZoom = true
+    controls.enableRotate = true
+
+    controls.rotateSpeed = 1.1
+    controls.zoomSpeed = 1.2
+
+}
